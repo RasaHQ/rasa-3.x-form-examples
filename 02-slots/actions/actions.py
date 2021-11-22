bdc610e661a657ff3dde337e8628baa44cecdea6
Vincent <vincentwarmerdam@gmail.com>
2021-11-22 16:40:09 +0100

    added

--- 02-slots/actions/actions.py
+++ 02-slots/actions/actions.py
@@ -5,20 +5,6 @@ from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
 
-class ActionReceiveShirtSize(Action):
-
-    def name(self) -> Text:
-        return "action_receive_shirt_size"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-        shirt_size = tracker.get_slot("shirt_size")
-        dispatcher.utter_message(text=f"I'll remember that your shirt size is {shirt_size}!")
-        return []
-
-
 class ActionSayShirtSize(Action):
 
     def name(self) -> Text:
