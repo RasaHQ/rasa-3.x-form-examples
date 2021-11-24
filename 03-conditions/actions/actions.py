83b2c67c58eeb3e63498ce480e4f9bf8b1be3207
Vincent <vincentwarmerdam@gmail.com>
2021-11-24 11:41:13 +0100

    added

---
+++ 03-conditions/actions/actions.py
@@ -0,0 +1,22 @@
+from typing import Any, Text, Dict, List
+
+from rasa_sdk.events import SlotSet
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class ActionSayShirtSize(Action):
+
+    def name(self) -> Text:
+        return "action_say_shirt_size"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        shirt_size = tracker.get_slot("shirt_size")
+        if not shirt_size:
+            dispatcher.utter_message(text="I don't know your shirt size.")
+        else:
+            dispatcher.utter_message(text=f"Your shirt size is {shirt_size}!")
+        return []
