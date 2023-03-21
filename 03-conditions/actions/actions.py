734fe0c0c58f927e32dac00b212bfd013c1af91e
vcidst <hello@shailendra.me>
2023-03-21 12:05:32 +0100

    update sample bot branch from testing

--- 03-conditions/actions/actions.py
+++ 03-conditions/actions/actions.py
@@ -5,6 +5,7 @@ from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
 
+# Custom action to say the shirt size with no SlotSet
 class ActionSayShirtSize(Action):
 
     def name(self) -> Text:
@@ -18,5 +19,26 @@ class ActionSayShirtSize(Action):
         if not shirt_size:
             dispatcher.utter_message(text="I don't know your shirt size.")
         else:
-            dispatcher.utter_message(text=f"Your shirt size is {shirt_size}!")
+            dispatcher.utter_message(text=f"[custom action says] Your shirt size is {shirt_size}!")
         return []
+
+
+# Custom action to update the shirt size with SlotSet and say it
+class ActionUpdateAndSayShirtSize(Action):
+
+    def name(self) -> Text:
+        return "action_update_and_say_shirt_size"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        shirt_size = tracker.get_slot("shirt_size")
+        if not shirt_size:
+            dispatcher.utter_message(text="I don't know your shirt size.")
+            return []
+        else:
+            shirt_size = "small"
+            dispatcher.utter_message(text=f"[custom action says] I've updated your shirt size to {shirt_size}!")
+            return [SlotSet("shirt_size", shirt_size)]
+        
